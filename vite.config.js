5369558b945a5a2ac266aca377e2de570eb55103
aymendakir <aymendakr@yahoo.com>
2024-08-02 18:13:10 +0100

    ds

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Movies_website/", // YOUR REPO NAME HERE
+  plugins: [react()]
 });
 
